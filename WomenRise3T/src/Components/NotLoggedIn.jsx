4a4e81ec41ed35d883f86ddf72f417e992a5ebbe
Terrana Willma <terrana.w@gmail.com>
2024-10-21 16:28:38 +0700

    create NotLoggedIn components

---
+++ WomenRise3T/src/Components/NotLoggedIn.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button, Container, Card, Row, Col } from 'react-bootstrap';
+
+
+const MentoringBeasiswaComponent = () => {
+    return (
+        <Container fluid>
+            <Row className="justify-content-center">
+                <Col xs={12} className="p-4" style={{ backgroundColor: '#F6FBFD' }}>
+                    <Card className="text-center border-0 m-4" style={{ backgroundColor: '#fff' }}>
+                        <Card.Body className="p-5">
+                            <Card.Title style={{ fontSize: '1.8rem', fontWeight: 'bold', color: '#004987' }}>
+                                Mentoring Beasiswa Perempuan 3T
+                            </Card.Title>
+                            <Card.Text>
+                                Bergabunglah dengan sesi mentoring khusus untuk Perempuan wilayah 3T!<br />
+                                Dapatkan bimbingan dari mentor berpengalaman dan wujudkan impianmu meraih beasiswa.<br />
+                                Daftar sekarang, kesempatan terbatas!
+                            </Card.Text>
+                            <div className="d-flex justify-content-center">
+                                <Button variant="primary" className="me-2">Log In</Button>
+                                <Button variant="secondary">Sign Up</Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default MentoringBeasiswaComponent;
+
