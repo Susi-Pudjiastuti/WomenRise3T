2931c8b1d9d4f9ce53654ea985de0361960cd3a6
EllenaAmanda <ellenaamanda846@gmail.com>
2024-11-03 22:36:52 +0700

    added desc motivation

--- src/Components/Landing/Motivasi.jsx
+++ src/Components/Landing/Motivasi.jsx
@@ -5,8 +5,8 @@ function Motivasi() {
     <div>
        <article className="background-lightblue border-bottom ">
         <div className="container px-5 px-md-0 py-5 text-center ">
-            <h2 className="text-center fw-bold button-style-text mb-4">Motivasi Untuk Kamu</h2>
-
+            <h2 className="text-center fw-bold button-style-text ">Motivasi Untuk Kamu</h2>
+            <p className='mb-4'>Dapatkan kutipan motivasi pribadi khusus untuk kamu! <span className='fw-bold fst-italic'>Tampilan berikut hanya contoh.</span></p>
             <div className='p-5 bg-white shadow-sm rounded mx-auto d-flex flex-column' style={{width: '75%'}}>
               <img src={petik} style={{width: '30px'}} />
               <p className='text-center mt-3 button-style-text fw-semibold'>Indah dari Lombok Barat, kamu memiliki kekuatan untuk melewati setiap tantangan, dan pendidikan adalah jalan menuju masa depan yang lebih baik. Sebagai perempuan dari 3T, perjalananmu penuh makna dan potensimu tak terbatas. Percayalah pada dirimu, jaga pikiran positif, dan biarkan tekadmu membuka jalan menuju kesuksesan. Setiap langkah kecilmu membawa perubahan besar.</p>
