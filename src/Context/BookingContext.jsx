4b12502a7a19e29060a12bdd36ede69befd228c6
EllenaAmanda <ellenaamanda846@gmail.com>
2024-11-06 22:48:32 +0700

    berhasil menambahkan booking untuk booking form ke dlm database

--- src/Context/BookingContext.jsx
+++ src/Context/BookingContext.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { createContext, useCallback, useEffect, useState } from 'react'
+import Swal from "sweetalert2";
 
 export const BookingContext = createContext();
 
@@ -10,7 +11,6 @@ function BookingProvider({ children }) {
     const [error, setError] = useState(null);
 
     //function untuk mengambil data booking user
-
     const getBookings = useCallback(async (mentorshipStatus) => {
         const token = localStorage.token;
         // console.log("token: " + token)
@@ -31,7 +31,7 @@ function BookingProvider({ children }) {
 
             const response = await axios.get(URL, {
                 headers: {
-                    'Authorization': `Bearer ${token}`, // Make sure `token` is defined in your context
+                    'Authorization': `Bearer ${token}`,
                 },
             });
             // console.log("Response data:", response.data.data);
@@ -56,12 +56,56 @@ function BookingProvider({ children }) {
             setLoading(false);
         } catch (error) {
             console.error('Error fetching mentorships:', error);
-            setError(e.message)
+            setError(error.message)
             setLoading(false)
         }
     }, []);
+
+    //menambahkan booking 
+    const addBooking = async(booking) => {
+        const token = localStorage.token;
+        console.log("token: " + token)
+        console.log(booking.namaPendaftar)
+        setLoading(true)
+
+        if (!token) {
+            console.error("Token not available or invalid");
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const response = await axios.post(`https://indirect-rosalind-rasunasaid1-522f984c.koyeb.app/bookings`, 
+                {
+                    namaPendaftar: booking.namaPendaftar,
+                    emailPendaftar: booking.emailPendaftar,
+                    noHp: booking.noHp,
+                    alasanMendaftar: booking.alasanMendaftar,
+                    mentorship: booking.mentorship
+                },
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    }
+                });
+            setMentorships(response.data.data);
+            setLoading(false);
+            Swal.fire({
+                title: "Booking Berhasil!",
+                text: "Anda telah terdaftar untuk sesi mentoring. Tunggu maksimal 1 x 24 jam anda akan  di-Invite ke WhatsApp Grup.",
+                icon: "success",
+              });
+        } catch (error) {
+            console.error('Error adding mentorships:', error);
+            setError(error.message)
+            setLoading(false)
+            //mengambil error message dari response
+            Swal.fire('Error', error.response?.data?.message || 'Failed to book mentorship class', 'error');
+        }
+    }
+
     return (
-        <BookingContext.Provider value={{ bookings, loading, error, getBookings, mentorships, fetchMentorships }}>
+        <BookingContext.Provider value={{ bookings, addBooking, loading, error, getBookings, mentorships, fetchMentorships }}>
             {children}
         </BookingContext.Provider>
     )
