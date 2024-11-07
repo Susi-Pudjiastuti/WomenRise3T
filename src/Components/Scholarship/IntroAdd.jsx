a3e284daec4617b9a23b044683dc9054c5f980ea
Terrana Willma <terrana.w@gmail.com>
2024-11-07 23:27:03 +0700

    add badge link

--- src/Components/Scholarship/IntroAdd.jsx
+++ src/Components/Scholarship/IntroAdd.jsx
@@ -7,7 +7,7 @@ const IntroAdd = () => {
         <li><strong>Jadilah bagian dari perubahan dengan menjadi <em>Volunteer</em> WomenRise3T!</strong> Tambahkan informasi beasiswa yang valid untuk membantu lebih banyak perempuan mendapatkan kesempatan pendidikan.</li>
         <li><strong>Tambahkan informasi beasiswa sebanyak-banyaknya.</strong> Semakin banyak informasi beasiswa yang kamu tambahkan, semakin banyak pula perempuan yang bisa terbantu.</li>
         <li><strong>Beasiswa yang kamu masukkan akan diverifikasi oleh tim admin WomenRise3T</strong> agar tetap relevan dan valid.</li>
-        <li><strong>Dapatkan <em>badge</em> Volunteer WomenRise3T!</strong> Hanya kontributor teraktif dan dengan informasi beasiswa yang valid yang akan menerima badge eksklusif ini, yang akan dikirim langsung ke email kamu.</li>
+        <li><strong>Dapatkan <a href="https://credsverse.com/credentials/a53cd014-68f2-43c6-a4fa-415f31c90e9b?recipient=true" target="_blank"><em>badge</em></a> Volunteer WomenRise3T!</strong> Hanya kontributor teraktif dan dengan informasi beasiswa yang valid yang akan menerima badge eksklusif ini, yang akan dikirim langsung ke email kamu.</li>
         <li><strong>Bangun dampak positif bersama kami</strong> dan bantu ciptakan peluang pendidikan lebih luas untuk perempuan di wilayah 3T!</li>
       </ul>
     </div>
